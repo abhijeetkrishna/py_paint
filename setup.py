d113bc97377cf154f13ff5b89c18239413619ba3
Abhijeet Krishna <abhijeet095@gmail.com>
2023-06-07 21:25:40 +0200

    convert to package

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='PyPaint',
+    version='1.0.0',
+    author='Abhijeet Krishna',
+    author_email='abhijeet095@gmail.com',
+    description='A package for mouse recording using GUI',
+    packages=find_packages(),
+    python_requires='>=3.6',
+)
